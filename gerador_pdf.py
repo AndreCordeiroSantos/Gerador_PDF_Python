0b25c9ed3d388e72dc7d299d85bfac7786fe3104
André Santos <115179571+AndreCordeiroSantos@users.noreply.github.com>
2023-05-30 15:25:29 -0300

    Gerador PDF
    Esse é um gerador em PDF, feito em Python, a ideia é criar uma ordem de serviço, para um cliente receber com os valores descritos.

---
+++ gerador_pdf.py
@@ -0,0 +1,42 @@
+from fpdf import FPDF 
+#!pip install fpdf - para instalar a biblioteca do pdf
+
+#inputs para pegar os valores digitados pelo usuário
+empresa = input('Informe a empresa: ')
+acesso = int(input('Quantos acessos?: '))
+visita = int(input('Quantas visitas?: '))
+
+#valores do serviços ja, pré definidos
+acesso_valor = 50
+visita_valor = 120
+
+#calculo do valor inserido do usuário e o valor pré definidos
+valor_total_acesso = acesso * acesso_valor
+valor_total_visita = visita * visita_valor
+
+#somando os dois valores e colocando na variável valor_total
+valor_total = int(valor_total_acesso) + int(valor_total_visita)
+
+#informações de todo o calculos na tela para o usuário verificar
+print(f'O valor total do acesso é?:  {valor_total_acesso}')
+print(f'O valor total da visita é? {valor_total_visita}')
+print(f'O valor total ficou em {valor_total}')
+  
+#comando para pré definir o pdf  
+pdf = FPDF()
+pdf.add_page()
+pdf.set_font("Arial")
+
+#utilizando um template
+pdf.image("template2.png", x=0, y=0 )
+
+# inserindo os dados do orçamento no pdf
+pdf.text(115, 145, str(empresa))
+pdf.text(115, 160, str(acesso))
+pdf.text(115, 175, str(visita))
+pdf.text(115, 205, str(valor_total))
+
+#mensagem de sucesso caso a geração do PDF de certo.
+pdf.output("Ordem de Serviços.pdf")
+print('Ordem Gerado com sucesso!')
+
